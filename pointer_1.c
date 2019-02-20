8fcd97e24c34b1a200c3e3751faafdd7885229ab
He-XiangPeng <hxpeng1989@hotmail.com>
2019-02-20 22:33:22 +0800

    modify pointer

--- pointer_1.c
+++ pointer_1.c
@@ -1,10 +1,15 @@
+/**
+ *	指针复习程序
+ *
+ * 
+ */
 #include <stdio.h>
-
 int main(void)
 {
 	int * p;
 	int i = 10;
 	p = &i;
-	printf("%d\n", i);
+	*p = 25;
+	printf("i = %d\n", i);
 	return 0;
 }
--- pointer_2.c
+++ pointer_2.c
@@ -1,7 +1,11 @@
+/**
+ *	数组的复习
+ *
+ * 
+ */
+
 #include <stdio.h>
-
 void show_Array(int * p, int len);
-
 int main(void)
 {
 	int a[5] = {1, 2, 3, 4, 5};
@@ -14,6 +18,11 @@ int main(void)
 	return 0;
 }
 
+/**
+ * 输出数组元素
+ * @param p   数组名
+ * @param len 数组长度
+ */
 void show_Array(int * p, int len)
 {
 	for (int i = 0; i < len; ++i)
